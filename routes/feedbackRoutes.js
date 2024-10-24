f13dde2796288fc568145fb1e31720bb5a88f4e9
Gustav Nikolai Degn <gude23ab@student.cbs.dk>
2024-10-24 18:47:46 +0200

    slet mvc

--- routes/feedbackRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const feedbackController = require("../controllers/feedbackController");
-
-// POST-rute til at indsende feedback
-router.post("/", feedbackController.submitFeedback);
-
-module.exports = router;
